6b8af81e55448288c3b428b0f1ac262b59da9b89
Tatianasvet <168006070+Tatianasvet@users.noreply.github.com>
2024-08-24 12:49:19 +0500

    answer

---
+++ module_13_3.py
@@ -0,0 +1,22 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.filters.command import Command
+import asyncio
+
+api = '7389491358:AAFBpmj2hCNKf_C4aKGzhnlmzU7_ztnojyk'
+bot = Bot(token=api)
+dp = Dispatcher(storage=MemoryStorage())
+
+@dp.message(Command("start"))
+async def start_message(message: types.Message):
+    await message.answer("Привет! Я бот помогающий твоему здоровью.")
+    print('Начинаем общение')
+@dp.message()
+async def other_message(message: types.Message):
+    await message.answer("Введите команду /start, чтобы начать общение.")
+    print("Нам пришло сообщение!")
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
